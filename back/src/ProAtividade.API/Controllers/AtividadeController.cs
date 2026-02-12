4f344657c90649ecf007ec6875b963100b867e7b
David Santos <davidwallacedossantos@gmail.com>
2026-02-11 22:08:53 -0300

    ASP.NET 10  WEBAPI

---
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Models;
+
+namespace ProAtividade.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AtividadeController : ControllerBase
+    {
+        public IList<Atividade> Atividades = new List<Atividade>()
+        {
+            new Atividade(1),
+            new Atividade(2),
+            new Atividade(3),
+        };
+
+        [HttpGet]
+        public IList<Atividade> Get()
+        {
+
+            return Atividades;
+        }
+
+        [HttpGet("{id}")]
+        public Atividade Get(int id)
+        {
+            var atividade = Atividades.FirstOrDefault(x => x.Id == id);
+            return atividade;
+        }
+
+        [HttpPost]
+        public IList<Atividade> Post(Atividade atividade)
+        {
+            Atividades.Add(atividade);
+            return Atividades; 
+        }
+
+        [HttpPut("{id}")]
+        public Atividade Put(int id, Atividade atividade)
+        {
+            atividade.Id = atividade.Id + 1;
+            return atividade;
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "My first method Delete";
+        }
+    }
+}
