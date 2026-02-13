b80095847391768f45e714401b6527456adc3afc
David Santos <davidwallacedossantos@gmail.com>
2026-02-13 16:56:57 -0300

    feat: Criando Contexto

--- back/src/ProAtividade.API/Program.cs
+++ back/src/ProAtividade.API/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProAtividade.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
